191ac8657066757d934136e544d9fed3ecfe760a
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2023-06-22 19:29:26 +0100

    feat(listen-messages): read messages from user

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const security = require('./security');
+
+const signingSecret = process.env.SLACK_SIGNING_SECRET;
+
 /**
  * 
  * @param {*} event
@@ -9,6 +13,17 @@
  * response must be compatible with json stringify 
  */
 exports.handler = (event, context, callback) => {
-    const response = {'message': 'Hello World'};
-    callback(null, response);
+    if (security.validateSlackRequest(event, signingSecret)){
+        const body = JSON.parse(event.body);
+        switch(body.type){
+            case "url_verification": callback(null, body.challenge); console.log(body); break;
+            case "event_callback": processRequest(body, callback); break;
+            default: callback(null);
+        }
+    }else callback("verfication failed");
+};
+
+const processRequest = (body, callback) => {
+    console.debug("message", body.event.text);
+    callback(null);
 }
