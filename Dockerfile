377ee9e5a14b3f45dfdaa38045f3625cd45236ba
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2023-06-24 15:05:43 +0100

    feat(db-write): Write to DynamoDb
    - Setup the table in AWS DynamoDb
    - Wrote push to db scripts
    - handled new mention type event in app.js

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM public.ecr.aws/lambda/nodejs:14
+FROM public.ecr.aws/lambda/nodejs:18
 # copy all js files and package-lock json files to
 # can also create docker ignore file and copy all
 COPY *.js package*.json /var/task/
--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const security = require('./security');
+const axios = require('axios');
+const db = require('./db');
 
 const signingSecret = process.env.SLACK_SIGNING_SECRET;
+const token = process.env.SLACK_BOT_TOKEN;
 
 /**
  * 
@@ -24,6 +27,43 @@ exports.handler = (event, context, callback) => {
 };
 
 const processRequest = (body, callback) => {
-    console.debug("message", body.event.text);
+    switch(body.event.type){
+        case "message": processMessages(body, callback); break;
+        case "app_mention": processAppMention(body, callback); break;
+        default: callback(null);
+    }
+}
+
+const processMessages = (body, callback) => {
+    console.debug("message:", body.event.text);
     callback(null);
+}
+
+const processAppMention = (body, callback) => {
+    const item = body.event.text.split(":").pop().trim();
+    db.saveItem(item, (error, result) => {
+        if(error!==null) {
+            callback(null);
+        } else {
+            const message = {
+                channel: body.event.channel,
+                text: `Item: \`${item}\` is saved to *Amazon DynamoDB*!"`
+            };
+            axios({
+                method: 'post',
+                url: 'https://slack.com/api/chat.postMessage',
+                headers: { 
+                    'Content-Type': 'application/json; charset=utf-8',
+                    'Authorization': `Bearer ${token}`
+                    },
+                data: message
+            })
+            .then((response) => {
+                callback(null);
+            })
+            .catch((error) => {
+                callback("failed to process app_mention");
+            });
+        }
+    })
 }
